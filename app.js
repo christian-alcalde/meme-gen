9a3cdaa6d41d221da075f8baf01aa7af2fe49c1c
Christian Alcalde <cc.alcalde@yahoo.com>
2022-02-26 20:12:00 -0800

    added ability to remove memes by clicking

--- app.js
+++ app.js
@@ -10,18 +10,31 @@ btn.addEventListener("click", function (e) {
   memeContainer.classList.add("meme");
   let img = document.createElement("img");
   img.src = url.value;
+
   let memeTop = document.createElement("div");
   memeTop.innerText = topText.value;
   memeTop.classList.add("meme-text", "top-text");
+
   let memeBottom = document.createElement("div");
   memeBottom.innerText = bottomText.value;
   memeBottom.classList.add("meme-text", "bottom-text");
 
-  memeContainer.append(img, memeTop, memeBottom);
+  let deleteButton = document.createElement("div");
+  let garbage = document.createElement("img");
+  garbage.src = "trash.svg";
+  garbage.classList.add("garbage");
+  deleteButton.append(garbage);
+
+  memeContainer.append(img, memeTop, memeBottom, deleteButton);
   memeSection.append(memeContainer);
 
   // clear text fields after submitting
   url.value = "";
   topText.value = "";
   bottomText.value = "";
+
+  // to remove meme
+  memeContainer.addEventListener("click", function (e) {
+    this.remove();
+  });
 });
