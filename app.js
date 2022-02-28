0049bea0461ee6bc7918d00061da95cdf06cc062
Christian Alcalde <cc.alcalde@yahoo.com>
2022-02-28 00:41:18 -0800

    cleaned up js code and fixed color transitions

--- app.js
+++ app.js
@@ -8,30 +8,16 @@ btn.addEventListener("click", function (e) {
   e.preventDefault(); // prevents button from refreshing page
   let memeContainer = document.createElement("div");
   memeContainer.classList.add("meme");
-  let img = document.createElement("img");
-  img.src = url.value;
 
-  let memeTop = document.createElement("div");
-  memeTop.innerText = topText.value;
-  memeTop.classList.add("meme-text", "top-text");
+  let img = addImage(url.value);
+  let text = addText(topText.value, bottomText.value);
+  let deleteButton = deleteOverlay();
 
-  let memeBottom = document.createElement("div");
-  memeBottom.innerText = bottomText.value;
-  memeBottom.classList.add("meme-text", "bottom-text");
-
-  let deleteButton = document.createElement("div");
-  let garbage = document.createElement("img");
-  garbage.src = "trash.svg";
-  garbage.classList.add("garbage");
-  deleteButton.append(garbage);
-
-  memeContainer.append(img, memeTop, memeBottom, deleteButton);
+  memeContainer.append(img, text[0], text[1], deleteButton);
   memeSection.append(memeContainer);
 
   // clear text fields after submitting
-  url.value = "";
-  topText.value = "";
-  bottomText.value = "";
+  clearInputs();
 
   // to remove meme
   memeContainer.addEventListener("click", function (e) {
@@ -39,15 +25,42 @@ btn.addEventListener("click", function (e) {
   });
 });
 
+let addImage = function (link) {
+  let img = document.createElement("img");
+  img.src = link;
+  return img;
+};
+
+let addText = function (top, bottom) {
+  let memeTop = document.createElement("div");
+  let memeBottom = document.createElement("div");
+
+  memeTop.innerText = top;
+  memeBottom.innerText = bottom;
+
+  memeTop.classList.add("meme-text", "top-text");
+  memeBottom.classList.add("meme-text", "bottom-text");
+  return [memeTop, memeBottom];
+};
+
+let deleteOverlay = function () {
+  let deleteButton = document.createElement("div");
+  let garbage = document.createElement("img");
+  garbage.src = "trash.svg";
+  garbage.classList.add("garbage");
+  deleteButton.append(garbage);
+  return deleteButton;
+};
+
+let clearInputs = () => {
+  url.value = "";
+  topText.value = "";
+  bottomText.value = "";
+};
+
 let h1 = document.querySelector("h1");
 let h2 = document.querySelector("h2");
 let header = document.querySelector(".meme-creation");
-
-setInterval(function () {
-  h1.style.color = randomColor();
-  h2.style.color = randomColor();
-  header.style.backgroundColor = randomColor();
-}, 500);
 
 function randomColor() {
   let r = Math.floor(Math.random() * 256);
@@ -55,3 +68,9 @@ function randomColor() {
   let b = Math.floor(Math.random() * 256);
   return `rgb(${r},${g},${b})`;
 }
+
+setInterval(function () {
+  h1.style.color = randomColor();
+  h2.style.color = randomColor();
+  header.style.backgroundColor = randomColor();
+}, 500);
